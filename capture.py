6537de5355d8c4813acdc013fbd0cdce566d59c9
Sahil Muthoo <sahil.muthoo@gmail.com>
2026-02-05 07:57:06 +0700

    match all request/response headers key=value

--- capture.py
+++ capture.py
@@ -6,8 +6,10 @@ def response(flow: http.HTTPFlow) -> None:
     entry = [
         flow.request.method,
         flow.request.pretty_url,
+        dict(flow.request.headers),
         flow.request.get_text(strict=False),
         flow.response.status_code,
+        dict(flow.response.headers),
         flow.response.get_text(strict=False),
     ]
 
--- spec/specmatic_spec.rb
+++ spec/specmatic_spec.rb
@@ -20,12 +20,26 @@ RSpec.describe "Specmatic" do
     expect(status.success?).to be(true), -> { "docker-compose failed:\n#{http_captured_lines}" }
 
     http_captured_lines.each_line do |entry|
-      method, url, request_body, status, response_body = JSON.parse(entry)
+      method, url, request_headers, request_body, status, response_headers, response_body = JSON.parse(entry)
 
       # ------------------------
       # HEAD /
       # ------------------------
       if method == "HEAD"
+        # request headers
+        expect(request_headers["User-Agent"]).to match(/^Java\//)
+        expect(request_headers["Host"]).to eq("host.docker.internal:9000")
+        expect(request_headers["Accept"]).to eq("text/html, image/gif, image/jpeg, */*; q=0.2")
+        expect(request_headers["Connection"]).to eq("keep-alive")
+
+        # response headers
+        expect(response_headers["Vary"]).to eq("Origin")
+        expect(response_headers["X-Specmatic-Result"]).to eq("failure")
+        expect(response_headers["X-Specmatic-Empty"]).to eq("true")
+        expect(response_headers["Content-Length"]).to eq("66")
+        expect(response_headers["Content-Type"]).to eq("text/plain")
+        expect(response_headers["Connection"]).to eq("keep-alive")
+
         expect(status).to eq(400)
         expect(response_body.to_s).to eq("")
         next
@@ -35,6 +49,23 @@ RSpec.describe "Specmatic" do
       # GET /swagger/v1/swagger.yaml
       # ------------------------
       if method == "GET" && url.include?("/swagger/v1/swagger.yaml")
+        # request headers
+        expect(request_headers["Host"]).to eq("host.docker.internal:9000")
+        expect(request_headers["Accept-Charset"]).to eq("UTF-8")
+        expect(request_headers["Accept"]).to eq("*/*")
+        expect(request_headers["User-Agent"]).to eq("Ktor client")
+        expect(request_headers["Content-Length"]).to eq("0")
+        expect(request_headers["Content-Type"]).to eq("text/plain")
+        expect(request_headers["Connection"]).to eq("Keep-Alive")
+
+        # response headers
+        expect(response_headers["Vary"]).to eq("Origin")
+        expect(response_headers["X-Specmatic-Result"]).to eq("failure")
+        expect(response_headers["X-Specmatic-Empty"]).to eq("true")
+        expect(response_headers["Content-Length"]).to eq("88")
+        expect(response_headers["Content-Type"]).to eq("text/plain")
+        expect(response_headers["Connection"]).to eq("keep-alive")
+
         expect(status).to eq(400)
         expect(response_body).to include("No matching REST stub")
         next
@@ -46,7 +77,17 @@ RSpec.describe "Specmatic" do
       if method == "POST" && url.end_with?("/widgets")
         expect(status).to eq(201)
 
-        # request
+        # request headers
+        expect(request_headers["Specmatic-Response-Code"]).to eq("201")
+        expect(request_headers["Host"]).to eq("host.docker.internal:9000")
+        expect(request_headers["Accept-Charset"]).to eq("UTF-8")
+        expect(request_headers["Accept"]).to eq("*/*")
+        expect(request_headers["User-Agent"]).to eq("Ktor client")
+        expect(request_headers["Content-Length"]).to eq(request_body.bytesize.to_s)
+        expect(request_headers["Content-Type"]).to eq("application/json")
+        expect(request_headers["Connection"]).to eq("Keep-Alive")
+
+        # request body
         req = JSON.parse(request_body)
         expect(req.keys).to contain_exactly("name", "type")
 
@@ -55,7 +96,15 @@ RSpec.describe "Specmatic" do
 
         expect(%w[mechanical hydraulic]).to include(req["type"])
 
-        # response
+        # response headers
+        expect(response_headers["Vary"]).to eq("Origin")
+        expect(response_headers["X-Specmatic-Result"]).to eq("success")
+        expect(response_headers["X-Specmatic-Type"]).to eq("random")
+        expect(response_headers["Content-Length"]).to eq(response_body.bytesize.to_s)
+        expect(response_headers["Content-Type"]).to eq("application/json")
+        expect(response_headers["Connection"]).to eq("keep-alive")
+
+        # response body
         res = JSON.parse(response_body)
         expect(res.keys).to eq(["id"])
         expect(res["id"]).to be_a(Integer)
@@ -69,6 +118,23 @@ RSpec.describe "Specmatic" do
       if method == "GET" && url.match?(%r{/widgets/\d+$})
         expect(status).to eq(200)
 
+        # request headers
+        expect(request_headers["Specmatic-Response-Code"]).to eq("200")
+        expect(request_headers["Host"]).to eq("host.docker.internal:9000")
+        expect(request_headers["Accept-Charset"]).to eq("UTF-8")
+        expect(request_headers["Accept"]).to eq("*/*")
+        expect(request_headers["User-Agent"]).to eq("Ktor client")
+        expect(request_headers["Connection"]).to eq("Keep-Alive")
+
+        # response headers
+        expect(response_headers["Vary"]).to eq("Origin")
+        expect(response_headers["X-Specmatic-Result"]).to eq("success")
+        expect(response_headers["X-Specmatic-Type"]).to eq("random")
+        expect(response_headers["Content-Length"]).to eq(response_body.bytesize.to_s)
+        expect(response_headers["Content-Type"]).to eq("application/json")
+        expect(response_headers["Connection"]).to eq("keep-alive")
+
+        # response body
         res = JSON.parse(response_body)
         expect(res.keys).to contain_exactly("id", "name", "type")
 
